d164bb770537f24ab1f9a3862d7c82ae41794838
Ben Hartshorne <ben@hound.sh>
2016-10-20 16:47:43 -0700

    bump honeytail to 1.207

--- honey_installer/honeytail_version.py
+++ honey_installer/honeytail_version.py
@@ -1,8 +1,8 @@
 # generated file - do not edit
 # instead update ../update_honeytail.sh
 import platform
-HONEYTAIL_VERSION="1.200"
+HONEYTAIL_VERSION="1.207"
 HONEYTAIL_CHECKSUM = {
-    "Linux": "daf4da2f2a55fca9d1bf413e0c8d66d36e8a046f7c382a2a29e0940e52c234bd",
+    "Linux": "4d82e2aed4a7c751ce2e97f236c511b328b13e5f4a6f202630990870958a7aac",
     #"Darwin": ""
 }.get(platform.system(), None)
